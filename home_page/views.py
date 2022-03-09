8b5d53c66cff0dbf7566386447196085e76e1801
tristan <tsoldenhoff@gmail.com>
2022-03-09 15:53:16 +0000

    personal portfolio 03/09/2022

--- home_page/views.py
+++ home_page/views.py
@@ -3,4 +3,4 @@ from .models import Project
 
 def home(request):
     projects = Project.objects.all()                                            #grab all objects from the database that are project objects
-    return render(request, 'home_page/home.html', {'projects':projects})        #refers and sends objects to html page
+    return render(request, 'main_site/index.html', {'projects':projects})        #refers and sends objects to html page
